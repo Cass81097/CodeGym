2c628bfd796efe4e44445ffd472de1eb2d764ffa
Hoang Anh <hoanganh81097@gmail.com>
2023-06-20 20:26:22 +0700

    ok

--- Module-3/nodeJS/bt2/app.js
+++ Module-3/nodeJS/bt2/app.js
@@ -1,43 +1,68 @@
 import http from 'http';
 import fs from 'fs'; 
 import qs from 'qs';
+import url from 'url';
 import { StudentServices } from './services/studentServices.js';
 
-let studentServices = new StudentServices()
+let studentServices = new StudentServices();
+
+const typeFile = {
+    'jpg': 'images/jpg',
+    'png': 'images/png',
+    'js': 'text/javascript',
+    'css': 'text/css',
+    'svg': 'image/svg+xml',
+    'ttf': 'font/tff',
+    'woff': 'font/woff',
+    'woff2': 'font/woff',
+    'eot': 'application/vnd.ms-fontobject'
+}
 
 const server = http.createServer((req, res) => {
-    if (req.method === 'GET') {
-        showList(req, res);
 
+    // file handle
+    const pathName = url.parse(req.url, true).pathname;
+    const checkPath = pathName.match(/\.js|\.css|\.png|\.jpg|\.ttf|\.woff|\.woff2|\.eot/);
+    if (checkPath) {
+        const contentType = typeFile[checkPath[0].toString().split('.')[1]];
+        res.writeHead(200, {'Content-Type': contentType});
+        fs.createReadStream(process.cwd() + req.url).pipe(res);
     } else {
-        let data = '';
-        req.on('data', (dataRaw) => {
-            data += dataRaw;
-        });
-        
-        req.on('end', () => {
-            const parsedData = qs.parse(data);
-            if (parsedData.search) {
-                const searchResults = studentServices.findStudentByName(parsedData.search);
-                renderHtml(res, searchResults);
-            } 
-
-            else if (parsedData.deleteId) {
-                const deleteResults = studentServices.deleteStudent(parsedData.deleteId);
-                renderHtml(res, deleteResults);
-            } 
-
-            else if (parsedData.id && parsedData.name && parsedData.score) {
-                const newData = { id: parsedData.id, name: parsedData.name, score: parsedData.score };
-                studentServices.addStudent(newData);
-                showList(req, res);
-            } 
-
-            else {
-                res.end('Loi!');
-            }
-        });
+        if (req.method === 'GET') {
+            showList(req, res);
+    
+        } else {
+            let data = '';
+            req.on('data', (dataRaw) => {
+                data += dataRaw;
+            });
+            
+            req.on('end', () => {
+                const parsedData = qs.parse(data);
+                if (parsedData.search) {
+                    const searchResults = studentServices.findStudentByName(parsedData.search);
+                    renderHtml(res, searchResults);
+                } 
+    
+                else if (parsedData.deleteId) {
+                    const deleteResults = studentServices.deleteStudent(parsedData.deleteId);
+                    renderHtml(res, deleteResults);
+                } 
+    
+                else if (parsedData.id && parsedData.name && parsedData.score) {
+                    const newData = { id: parsedData.id, name: parsedData.name, score: parsedData.score };
+                    studentServices.addStudent(newData);
+                    showList(req, res);
+                } 
+    
+                else {
+                    res.end('Loi!');
+                }
+            });
+        }
     }
+
+    
 });
 
 function showList(req, res) {
@@ -60,4 +85,3 @@ function renderHtml(res, items) {
 server.listen('8080', () =>{
     console.log('Kết nối thành công');
 });
-
