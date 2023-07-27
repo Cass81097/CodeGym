39146b36f955d8f728f38e33505f3f0668c98073
Hoang Anh <hoanganh81097@gmail.com>
2023-07-27 16:04:20 +0700

    convert to async/await

--- Module-4/qlsv - layout/views/js/classes/homeClass.js
+++ Module-4/qlsv - layout/views/js/classes/homeClass.js
@@ -1,29 +1,31 @@
-function loadHome() {
+async function loadHome() {
     let str = `
         <div id="home"></div> 
         <div id="main"></div>
-        `;
+    `;
     let home = `
-             <span style="color:red" onclick="loadHome()">Home</span> | <span onclick="loadAddClass()">Add Class</span> | <span onclick="loadEditClass()">Edit Class</span> | <span onclick="loadDeleteClass()">Delete Class</span> | <span style="color:blue" onclick="loadStudent()">List Student</span>
-             `
+        <span style="color:red" onclick="loadHome()">Home</span> | 
+        <span onclick="loadAddClass()">Add Class</span> | 
+        <span onclick="loadEditClass()">Edit Class</span> | 
+        <span onclick="loadDeleteClass()">Delete Class</span> | 
+        <span style="color:blue" onclick="loadStudent()">List Student</span>
+    `;
     document.getElementById(`display`).innerHTML = str;
     document.getElementById(`home`).innerHTML = home;
 
-    axios.get('http://localhost:3000/classes')
-        .then((res) => {
-            let data = res.data;
-            let str = '';
-            data.map(item => {
-                str += `<h3>${item.id}. Class: ${item.name} - Teacher: ${item.teacherName} - Total Student: ${item.totalStudent}</h3>`;
-            });
-            document.getElementById('main').innerHTML = str;
-        })
-        .catch((error) => {
-            console.error(error);
+    try {
+        const res = await axios.get('http://localhost:3000/classes');
+        let data = res.data;
+        let str = '';
+        data.map(item => {
+            str += `<h3>${item.id}. Class: ${item.name} - Teacher: ${item.teacherName} - Total Student: ${item.totalStudent}</h3>`;
         });
+        document.getElementById('main').innerHTML = str;
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 function loadClass() {
     loadHome();
 }
-
--- Module-4/qlsv - layout/views/js/students/deleteStudent.js
+++ Module-4/qlsv - layout/views/js/students/deleteStudent.js
@@ -1,4 +1,4 @@
-function loadDeleteStudent() {
+async function loadDeleteStudent() {
     let str = `
         <div id="home"></div> 
         <div id="main"></div>
@@ -9,25 +9,27 @@ function loadDeleteStudent() {
     document.getElementById(`display`).innerHTML = str;
     document.getElementById(`home`).innerHTML = home;
 
-    axios.get('http://localhost:3000/students')
-        .then((res) => {
-            let data = res.data;
-            let str = '';
-            data.map(item => {
-                str += `<h3>${item.id}. Name: ${item.name} - Age: ${item.age} - Point: ${item.point} - Class: ${item.classRef.name}</h3>
-                        <button onclick="deleteStudent(${item.id})">Delete</button>
-                `;
-            });
-            document.getElementById('main').innerHTML = str;
-        })
-        .catch((error) => {
-            console.error(error);
+    try {
+        const res = await axios.get('http://localhost:3000/students');
+        let data = res.data;
+        let str = '';
+        data.map(item => {
+            str += `<h3>${item.id}. Name: ${item.name} - Age: ${item.age} - Point: ${item.point} - Class: ${item.classRef.name}</h3>
+                    <button onclick="deleteStudent(${item.id})">Delete</button>
+            `;
         });
+        document.getElementById('main').innerHTML = str;
+    } catch (error) {
+        console.error(error);
+    }
 }
 
-function deleteStudent(id) {
-    axios.delete(`http://localhost:3000/students/delete?id=${id}`).then(res => {
+async function deleteStudent(id) {
+    try {
+        const res = await axios.delete(`http://localhost:3000/students/delete?id=${id}`);
         console.log(res);
         loadStudent();
-    })
+    } catch (error) {
+        console.error(error);
+    }
 }
