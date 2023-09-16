311137d43c88dbae7e7f6ddb05c5d799d07b6cad
Hoang Anh <hoanganh81097@gmail.com>
2023-09-16 19:34:07 +0700

    push

---
+++ Module-6/case-socialmedia/server/controller/UserController.ts
@@ -0,0 +1,52 @@
+import {Request, Response} from "express";
+import userService from "../service/UserService";
+import cookie from 'cookie';
+const session = require('express-session');
+
+class UserController {
+
+    // register = async (req: Request, res: Response) => {
+    //    let result = await userService.register(req.body);
+    //     res.json(result)
+    // }
+
+    // login = async (req: Request, res: Response) => {
+    //     let resultCheck = await userService.checkUser(req.body); 
+    //     res.status(200).json(resultCheck);
+    // }
+
+    register = async (req: Request, res: Response) => {
+        await userService.register(req.body);
+        res.status(201).json('Create user success')
+    }
+
+    login = async (req: Request, res: Response) => {
+        let resultCheck = await userService.checkUser(req.body);
+        res.status(200).json(resultCheck);   
+    }
+    
+    check = async (req: Request, res: Response) => {
+        console.log(req.body)
+        let check = await userService.checkLogin(req.body.username,req.body.password)
+        res.json(check)
+    }
+
+    findAll = async (req: Request, res: Response) => {
+        let {id, name, email} = req.query
+        if (id == undefined && name == undefined && email == undefined ){
+            let data = await userService.findAll()
+            res.json(data)
+        } else if (id != undefined && name == undefined && email == undefined ) {
+            let data = await userService.findById(id)
+            res.json(data)
+        } else if (id == undefined && name != undefined && email == undefined){
+            let data = await userService.findByName(name)
+            res.json(data)
+        } else if (id == undefined && name == undefined && email != undefined){
+            let data = await userService.findByEmail(email)
+            res.json(data)
+        }
+    }
+}
+
+export default new UserController();
---
+++ Module-6/case-socialmedia/server/service/UserService.ts
@@ -0,0 +1,115 @@
+import { User } from "../entity/user";
+import { AppDataSource } from "../data-source";
+import { Like } from "typeorm";
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import { SECRET } from "../middleware/jwt";
+
+class UserService {
+    private userRepository;
+
+    constructor() {
+        this.userRepository = AppDataSource.getRepository(User);
+    }
+
+    // register = async (user: User) => {
+    //     let userObj = await this.userRepository.findOne({
+    //         where: {
+    //             username: user.username
+    //         }
+    //     });
+
+    //     if (userObj && user.username === userObj.username) {
+    //         return `Account '${user.username}' already exists`;
+    //     } else {
+    //         return this.userRepository.save(user);
+    //     }
+    // }
+
+    // checkUser = async (user) => {
+    //     let userFind = await this.userRepository.findOne({
+    //         where: {
+    //             username: user.username,
+    //         },
+    //     });
+
+    //     if (!userFind) {
+    //         return 'User does not exist';
+    //     } else {
+    //         if (user.password == userFind.password) {
+    //             const userId = userFind.id; // Lấy ra ID của người dùng
+    //             return `${userId}`;
+    //         } else {
+    //             return 'Password is wrong';
+    //         }
+    //     }
+    // };
+
+    register = async (user) => {
+        user.password = await bcrypt.hash(user.password, 10);
+        return this.userRepository.save(user);
+    }
+
+    checkLogin = async (username,password) =>{
+      const row = await this.userRepository.find({
+            where : {
+                username : username,
+                password : password
+            }
+        })
+        return row.length === 1;
+    }
+
+    checkUser = async (user) => {
+        let userFind = await this.userRepository.findOneBy({username: user.username});
+        if (!userFind) {
+            return 'User is not exist'
+        } else {
+            let passWordCompare = await bcrypt.compare(user.password, userFind.password);
+            if (passWordCompare) {
+                let payload = {
+                    idUser: userFind.id,    
+                    username: userFind.username,
+                    password: userFind.password,
+                    // role: 'admin'
+                }
+                return jwt.sign(payload, SECRET, {
+                    expiresIn: 36000 * 10 * 100
+                })
+            } else {
+                return 'Password is wrong'
+            }
+        }
+    }
+
+
+    findAll = async () => {
+        return this.userRepository.find()
+    }
+
+    findById = async (id) => {
+        return this.userRepository.find({
+            where: {
+                id: id
+            }
+        })
+    }
+
+    findByName = async (name) => {
+        return this.userRepository.find({
+            where: {
+                username: Like(`%${name}%`)
+            }
+        });
+    }
+
+    findByEmail = async (email) => {
+        return this.userRepository.find({
+          where: {
+            email: email
+          }
+        });
+    }
+}
+
+export default new UserService();
