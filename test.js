d31b4b9625797c01b68867f5e55f23e0edf54a0d
Cass81097 <truonghoanganh.810@gmail.com>
2023-10-06 10:11:32 +0700

    push picture

--- test.js
+++ test.js
@@ -1,25 +1,2 @@
-let s1 = 'aabcc';
-let s2 = 'adcaa';
-let arrC = []
-
-let foundMatch = false;
-
-for (let i = 1; i <= s1.length; i++) {
-    let currentCharA = s1[i];
-
-    for (let j = 0; j < s2.length; j++) {
-        if (currentCharA === s2[j]) {
-            foundMatch = true;
-            break;
-        }
-    }
-
-    if (foundMatch) {
-        // console.log([i]);
-        arrC.push(i)
-    }
-
-    foundMatch = false;
-}
-
-return arrC.length;
+const testForEach = [1, 2, 3];
+testForEach.forEach(test => console.log(test))
